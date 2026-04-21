15d9b43f15e3a4a9baf3461b0c99561396448882
SebastianMonjePulecio <monjesebas7@gmail.com>
2026-04-21 10:26:17 -0500

    update page

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "IT Support | Data Analyst | Junior Developer";
+let i = 0;
+
+function typingEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingEffect, 50);
+  }
+}
+
+typingEffect();
